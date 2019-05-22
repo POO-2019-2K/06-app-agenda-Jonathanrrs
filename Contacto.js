629d27fd52af2cd9f5a434cf652863dc3ac21ab5
Jonathanrrs12 <yoni_12rrs@hotmail.com>
2019-05-21 19:39:58 -0500

    version1

---
+++ Contacto.js
@@ -0,0 +1,44 @@
+export default class Contacto {
+    constructor(contacto) {
+        this._nombre = contacto.nombre;
+        this._fechaNac = contacto.fechaNac;
+        this._correo = contacto.correo;
+        this._mes = [
+            "Ene",
+            "Feb",
+            "Mar",
+            "Abr",
+            "May",
+            "Jun",
+            "Jul",
+            "Ago",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dic"
+          ];
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+    get fechaNac() {
+        return this._fechaNac;
+    }
+    get correo() {
+        return this._correo;
+    }
+    getNacimiento() {
+        let fecha = this._fechaNac.getDate() + "/" + this._mes[this._fechaNac.getMonth()] + "/" + this._fechaNac.getFullYear();
+        return fecha;
+    }
+    getEdad() {
+        let oneDay = 24 * 60 * 60 * 1000;
+        let oneYear = oneDay * 365;
+        let differenceMs = new Date() - this._fechaNac;
+        let edad = Math.trunc(differenceMs / oneYear);
+        return edad;
+    }
+
+
+}
