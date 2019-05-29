9e61c40ff40097540613549177842996dc1adf78
Jonathanrrs12 <yoni_12rrs@hotmail.com>
2019-05-28 23:31:22 -0500

    boton borrar

--- Agenda.js
+++ Agenda.js
@@ -5,19 +5,41 @@ export default class Agenda {
         this._tablaAgenda = tablaAgenda;
         this._contactos = [];
         this._initTables();
+        //localStorage.removeItem("contactos");
     }
 
     _initTables() {
         let contactos = JSON.parse(localStorage.getItem("contactos"));
-        if(contactos === null) {
+        if (contactos === null) {
             return;
         }
-        contactos.forEach((contacto,index) => {
+        contactos.forEach((contacto, index) => {
             contacto.fechaNac = new Date(contacto.fechaNac);
             this._showInTable(new Contacto(contacto));
-        })
+        });
+    }
+    _borrar(row,contacto,email) {
+        let btnBorrar = document.createElement("input");
+        btnBorrar.type = "button";
+        btnBorrar.value = "Borrar";
+        btnBorrar.className = "btn btn-danger";
+        row.cells[4].innerHTML = "";
+        row.cells[4].appendChild(btnBorrar);
+        btnBorrar.addEventListener("click", () => {
+            this.bBorrarRow(contacto);
+        });
     }
 
+    _borrarRow(contacto) {
+        this._contactos = JSON.parse(localStorage.getItem("contactos"));
+        this._contactos.forEach((e, index) => {
+            if (e.nombre === contacto.nombre) {
+                this._contactos.splice(index, 1);
+            }
+        });
+        location.reload();
+        localStorage.setItem("contactos", JSON.stringify(this._contactos));
+    }
     _showInTable(contacto) {
         let row = this._tablaAgenda.insertRow(-1);
 
@@ -25,18 +47,22 @@ export default class Agenda {
         let cellFechaNac = row.insertCell(1);
         let cellCorreo = row.insertCell(2);
         let cellEdad = row.insertCell(3);
+        row.insertCell(4);
 
         cellNombre.innerHTML = contacto.nombre;
         cellFechaNac.innerHTML = contacto.getNacimiento();
         cellCorreo.innerHTML = contacto.correo;
         cellEdad.innerHTML = contacto.getEdad();
+        this._borrar(row, contacto);
 
         let objContacto = {
             nombre: contacto.nombre,
             fechaNac: contacto.fechaNac,
             correo: contacto.correo
+
         }
         this._contactos.push(objContacto);
+
     }
 
     addContacto(contacto) {
