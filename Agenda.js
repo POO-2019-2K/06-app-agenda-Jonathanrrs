f11d1cbb4ff84b6ae20efd2f62c033d6d4a27518
Jonathan Ramiro Ruiz Sandoval <yoni_12rrs@hotmail.com>
2019-05-30 01:56:15 -0500

    fin

--- Agenda.js
+++ Agenda.js
@@ -1,14 +1,10 @@
 import Contacto from "./Contacto.js";
-
 export default class Agenda {
     constructor(tablaAgenda) {
         this._tablaAgenda = tablaAgenda;
         this._contactos = [];
         this._initTables();
-       
-        //localStorage.removeItem("contactos");
     }
-
     _initTables() {
         let contactos = JSON.parse(localStorage.getItem("contactos"));
         if (contactos === null) {
@@ -17,37 +13,43 @@ export default class Agenda {
         contactos.forEach((contacto, index) => {
             contacto.fechaNac = new Date(contacto.fechaNac);
             this._showInTable(new Contacto(contacto));
-            console.log(contacto.fechaNac)
-            
         });
     }
-    
     _showInTable(contacto) {
         let row = this._tablaAgenda.insertRow(-1);
-
         let cellNombre = row.insertCell(0);
         let cellFechaNac = row.insertCell(1);
         let cellCorreo = row.insertCell(2);
         let cellEdad = row.insertCell(3);
-        row.insertCell(4);
+        let cellNumero = row.insertCell(4);
+        row.insertCell(5);
 
         cellNombre.innerHTML = contacto.nombre;
         cellFechaNac.innerHTML = contacto.getNacimiento();
         cellCorreo.innerHTML = contacto.correo;
         cellEdad.innerHTML = contacto.getEdad();
+        cellNumero.innerHTML = contacto.numero;
         this._borrar(row, contacto);
 
         let objContacto = {
             nombre: contacto.nombre,
             fechaNac: contacto.fechaNac,
-            correo: contacto.correo
-
+            correo: contacto.correo,
+            numero: contacto.numero
         }
         this._contactos.push(objContacto);
-        
     }
 
     addContacto(contacto) {
+        let found = this._findContacto(contacto.correo);
+        if (found >= 0) {
+            Swal.fire({
+                type: "error",
+                tittle: "Error",
+                text: "El contacto ya existe"
+            });
+            return;
+        }
         this._showInTable(contacto);
         localStorage.setItem("contactos", JSON.stringify(this._contactos));
     }
@@ -60,9 +62,8 @@ export default class Agenda {
             return 1;
         }
         return 0;
-
     }
-    _compararNum(a,b) {
+    _compararNum(a, b) {
         if (a.fechaNac > b.fechaNac) {
             return -1;
         }
@@ -76,18 +77,15 @@ export default class Agenda {
         this._contactos.sort(this._compararAlfa);
     }
     _num() {
-        console.log(this._contactos.sort(this._compararNum))
         this._contactos.sort(this._compararNum);
-       
     }
-    
+
     mostrarAlfa() {
         this._contactos.sort(this._compararAlfa);
         localStorage.setItem("contactos", JSON.stringify(this._contactos));
         location.reload();
     }
     mostrarNum() {
-      
         this._contactos.sort(this._compararNum);
         localStorage.setItem("contactos", JSON.stringify(this._contactos));
         location.reload();
@@ -97,8 +95,8 @@ export default class Agenda {
         btnBorrar.type = "button";
         btnBorrar.value = "Borrar";
         btnBorrar.className = "btn btn-danger";
-        row.cells[4].innerHTML = "";
-        row.cells[4].appendChild(btnBorrar);
+        row.cells[5].innerHTML = "";
+        row.cells[5].appendChild(btnBorrar);
         btnBorrar.addEventListener("click", () => {
             this._borrarRow(contacto);
         });
@@ -114,5 +112,14 @@ export default class Agenda {
         location.reload();
         localStorage.setItem("contactos", JSON.stringify(this._contactos));
     }
-
+    _findContacto(correo) {
+        let foundAt = -1;
+        this._contactos.forEach((e, index) => {
+            if (e.correo === correo) {
+                foundAt = index;
+                return;
+            }
+        });
+        return foundAt;
+    }
 }
